bba8cd7e4cf3048889571bce7265c57e4b17a44e
cs4372 <cshing4372@gmail.com>
2023-01-05 22:17:08 -0500

    Bucket-List-Hotels: move mock data to new file and add delete acction

--- Bucket-List-Hotels/Bucket-List-Hotels/Controller/HotelTableViewController.swift
+++ Bucket-List-Hotels/Bucket-List-Hotels/Controller/HotelTableViewController.swift
@@ -8,13 +8,6 @@
 import UIKit
 
 class HotelTableViewController: UITableViewController {
-    var hotels: [Hotel] = [
-        Hotel(name: "Andaz Amsterdam", address: "Prinsengracht 587, 1016 HT Amsterdam, Netherlands", country: "Netherlands", phone: "+31 20 523 1234", description: "Andaz Amsterdam Prinsengracht was formerly the Public Library and is now one of the most creative five star luxury-lifestyle hotels in the city. A stay at Andaz Amsterdam is a dream for every design lover looking for an unforgettable local experience.", stars: "5 stars", image: "andaz"),
-        Hotel(name: "Four Seasons Hotel Gresham Palace Budapest", address: "Rue Abou Abbas El Sebti، 40000, Morocco", country: "Morocco", phone: "+212 52980-8080", description: "Located in the heart of Budapest, our art nouveau palace Hotel embodies the spirit of Budapest’s Golden Era glam. From the moment you enter, savour the iconic wrought-iron peacock gates, pass through the Lobby adorned with over two million mosaic tiles illuminated with its striking chandelier, and let inspiration wash over you.", stars: "5 stars", image: "fourseasons_budapest"),
-        Hotel(name: "Royal Mansour Marrakech", address: "Budapest, Széchenyi István tér 5-6, 1051 Hungary", country: "Hungary", phone: "+36 1 268 6000", description: "The Royal Mansour can be counted amongst the few establishments in the world that defy hotel excellence. Just a few steps away from the famous Jemaa El Fna square beats the heart of the Marrakesh Palace, offering travellers the unique concept of a medina within a medina.", stars: "5 stars", image: "royal_mansour"),
-        Hotel(name: "The Silo", address: "Silo Square, Victoria & Alfred Waterfront, Cape Town, 8001, South Africa", country: "South Africa", phone: "+27 21 670 0500", description: "The Silo is a sanctuary in the city celebrating art, style, and design. This architectural masterpiece towers above the V&A Waterfront and is defined by its shimmering pillowed-glass windows which offer panoramic views stretching from Table Mountain to the Table Bay harbour.", stars: "5 stars", image: "the_silo")
-    ]
-    
     lazy var dataSource = setupDataSource()
 
     override func viewDidLoad() {
@@ -42,13 +35,30 @@ class HotelTableViewController: UITableViewController {
         )
         return dataSource
     }
-
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        guard let hotel = self.dataSource.itemIdentifier(for: indexPath)
+            else {
+                return UISwipeActionsConfiguration()
+            }
+            let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { action, view, completionHandler in
+                var snapshot = self.dataSource.snapshot()
+                snapshot.deleteItems([hotel])
+                self.dataSource.apply(snapshot, animatingDifferences: true)
+                // Call completion handler to dismiss the action button
+                completionHandler(true)
+            }
+            deleteAction.backgroundColor = .red
+            let configuration = UISwipeActionsConfiguration(actions: [deleteAction, deleteAction])
+            configuration.performsFirstActionWithFullSwipe = true
+            return configuration
+        }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showHotelContent" {
             if let indexPath = tableView.indexPathForSelectedRow {
                 let destinationController = segue.destination as! HotelContentViewController
-                destinationController.hotel = self.hotels[indexPath.row]
+                destinationController.hotel = hotels[indexPath.row]
             }
         }
     }
---
+++ Bucket-List-Hotels/Bucket-List-Hotels/MockData.swift
@@ -0,0 +1,15 @@
+//
+//  MockData.swift
+//  Bucket-List-Hotels
+//
+//  Created by Catherine Shing on 1/5/23.
+//
+
+import UIKit
+
+var hotels: [Hotel] = [
+    Hotel(name: "Andaz Amsterdam", address: "Prinsengracht 587, 1016 HT Amsterdam, Netherlands", country: "Netherlands", phone: "+31 20 523 1234", description: "Andaz Amsterdam Prinsengracht was formerly the Public Library and is now one of the most creative five star luxury-lifestyle hotels in the city. A stay at Andaz Amsterdam is a dream for every design lover looking for an unforgettable local experience.", stars: "5 stars", image: "andaz"),
+    Hotel(name: "Four Seasons Hotel Gresham Palace Budapest", address: "Rue Abou Abbas El Sebti، 40000, Morocco", country: "Morocco", phone: "+212 52980-8080", description: "Located in the heart of Budapest, our art nouveau palace Hotel embodies the spirit of Budapest’s Golden Era glam. From the moment you enter, savour the iconic wrought-iron peacock gates, pass through the Lobby adorned with over two million mosaic tiles illuminated with its striking chandelier, and let inspiration wash over you.", stars: "5 stars", image: "fourseasons_budapest"),
+    Hotel(name: "Royal Mansour Marrakech", address: "Budapest, Széchenyi István tér 5-6, 1051 Hungary", country: "Hungary", phone: "+36 1 268 6000", description: "The Royal Mansour can be counted amongst the few establishments in the world that defy hotel excellence. Just a few steps away from the famous Jemaa El Fna square beats the heart of the Marrakesh Palace, offering travellers the unique concept of a medina within a medina.", stars: "5 stars", image: "royal_mansour"),
+    Hotel(name: "The Silo", address: "Silo Square, Victoria & Alfred Waterfront, Cape Town, 8001, South Africa", country: "South Africa", phone: "+27 21 670 0500", description: "The Silo is a sanctuary in the city celebrating art, style, and design. This architectural masterpiece towers above the V&A Waterfront and is defined by its shimmering pillowed-glass windows which offer panoramic views stretching from Table Mountain to the Table Bay harbour.", stars: "5 stars", image: "the_silo")
+]
