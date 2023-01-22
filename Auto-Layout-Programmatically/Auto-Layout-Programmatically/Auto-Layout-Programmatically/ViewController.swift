9d9b875cdcf10417463bbad06625155cae0a2fab
cs4372 <cshing4372@gmail.com>
2023-01-22 18:47:47 -0500

    Auto-Layout-Programmatically: add code

---
+++ Auto-Layout-Programmatically/Auto-Layout-Programmatically/Auto-Layout-Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto-Layout-Programmatically
+//
+//  Created by Catherine Shing on 1/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
