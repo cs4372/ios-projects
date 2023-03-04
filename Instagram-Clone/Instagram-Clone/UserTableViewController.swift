a3b5b955a8994745e847357016380950ef8c0b9b
cs4372 <cshing4372@gmail.com>
2023-03-03 21:20:45 -0500

    Add ability to follow/unfollow users

--- Instagram-Clone/Instagram-Clone/UserTableViewController.swift
+++ Instagram-Clone/Instagram-Clone/UserTableViewController.swift
@@ -10,26 +10,46 @@ import Parse
 
 class UserTableViewController: UITableViewController {
     
-    var usernames = [""]
+    var usernames = [String]()
+    var objectIds = [String]()
+    var followings = [String:Bool]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let query = PFUser.query()
+        query?.whereKey("username", notEqualTo: PFUser.current()?.username)
         query?.findObjectsInBackground(block: {(users, error) in
             if error != nil {
                 
             } else if let users = users {
                 for obj in users {
+                    print("user", obj)
                     if let user = obj as? PFUser {
                         if let username = user.username {
-                            self.usernames.append(username)
+                            if let objectId = user.objectId {
+                                self.usernames.append(username)
+                                self.objectIds.append(objectId)
+                                
+                                let query = PFQuery(className:"Following")
+                                query.whereKey("follower", equalTo: PFUser.current()?.objectId)
+                                query.whereKey("following", equalTo: objectId)
+                                query.findObjectsInBackground(block: {(objects, err) in
+                                    if let objects = objects {
+                                        if objects.count > 0 {
+                                            self.followings[objectId] = true
+                                        } else {
+                                            self.followings[objectId] = false
+                                        }
+                                        self.tableView.reloadData()
+                                    }
+                                })
+                            }
                         }
                     }
                 }
             }
         })
-        self.tableView.reloadData()
     }
     
     @IBAction func logout(_ sender: Any) {
@@ -43,11 +63,54 @@ class UserTableViewController: UITableViewController {
         return 1
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return usernames.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = "Test"
+        cell?.textLabel?.text = usernames[indexPath.row]
+        if let followingBool = followings[objectIds[indexPath.row]] {
+            if followingBool {
+                cell?.accessoryType = UITableViewCell.AccessoryType.checkmark
+            }
+        } else {
+            cell?.accessoryType = UITableViewCell.AccessoryType.none
+        }
         return cell!
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        print(followings[objectIds[indexPath.row]])
+        if let followingsBool = followings[objectIds[indexPath.row]] {
+            if followingsBool {
+                cell?.accessoryType = UITableViewCell.AccessoryType.none
+                followings[objectIds[indexPath.row]] = false
+                let following = PFQuery(className:"Following")
+                following.whereKey("follower", equalTo: PFUser.current()?.objectId)
+                following.whereKey("following", equalTo: objectIds[indexPath.row])
+                following.findObjectsInBackground(block: {(objects, err) in
+                    if let objects = objects {
+                        for obj in objects {
+                            obj.deleteInBackground()
+                        }
+                    }
+                })
+            }
+         else {
+            cell?.accessoryType = UITableViewCell.AccessoryType.checkmark
+            followings[objectIds[indexPath.row]] = true
+            let following = PFObject(className:"Following")
+            following["follower"] = PFUser.current()?.objectId
+            following["following"] = objectIds[indexPath.row]
+             following.saveInBackground { (succeeded, error)  in
+                 if (succeeded) {
+                     print("following created!")
+                 } else {
+                     print("error selecting row")
+                 }
+                }  
+            }
+        }
+    }
 }
+
